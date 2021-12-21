708a11d626942a81869f8699844e17f13fd4b92a
Joseph Martin <josephmartinux@gmail.com>
2021-12-21 01:36:40 -0800

    Updates

---
+++ component---src-pages-tickets-js-e3c86462a55753918d64.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[528],{7254:function(e,t,a){a.r(t);var n=a(7294),l=a(5444),r=a(1500),c=a(3751);t.default=function(){return n.createElement(r.Z,null,n.createElement(c.Z,{title:"Tickets"}),n.createElement("h1",null,"Tickets Coming Soon!"),n.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-tickets-js-e3c86462a55753918d64.js.map
